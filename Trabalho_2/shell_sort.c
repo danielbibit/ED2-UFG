10418dbcb279da3835d4b0648294a6b352afabcb
pedrodiehl <pedroramosdiehl@gmail.com>
2017-09-10 20:08:32 -0300

    Alteração no trabalho 2 pra adequação do moodle

--- Trabalho_2/shell_sort.c
+++ Trabalho_2/shell_sort.c
@@ -1,5 +1,20 @@
+/*Alunos:
+	Daniel Moraes dos Santos  201602449
+	Pedro Ramos Krauze Diehl  201611880
+*/
+
 #include <stdio.h>
 
+int v[20] = {10, 15, 0, 6, 13, 11, 2, 8, 7, 4, 14, 18, 3, 17, 19, 12, 9, 1, 5, 16};
+
+void print_array(int *p, int len){
+	int i = 0;
+	printf("{");
+	for(i = 0; i < len; i++){
+		printf("%i,", p[i]);
+	}
+	printf("}\n");
+}
 void shell_sort(int *p, int len, int debug){
 	int gap = 1;
 	int i, j, value;
@@ -8,9 +23,8 @@ void shell_sort(int *p, int len, int debug){
 		gap = gap * 3 + 1;
 
 	while(gap >= 1){
-
 	gap /= 3;
-
+	printf("Gap = %i\n",gap);
 		for(i = gap; i < len; i++){
 			value = p[i];
 			j = i - gap;
@@ -19,18 +33,12 @@ void shell_sort(int *p, int len, int debug){
 				j -= gap;
 			}
 			p[j + gap] = value;
+			print_array(v,20);
 		}
 	}
 }
 
-void print_array(int *p, int len){
-	int i = 0;
-	for(i = 0; i < len; i++)
-		printf("%i,", p[i]);
-}
-
 int main(){
-	int v[20] = {10, 15, 0, 6, 13, 11, 2, 8, 7, 4, 14, 18, 3, 17, 19, 12, 9, 1, 5, 16};
 
 	shell_sort(v, 20, 0);
 
