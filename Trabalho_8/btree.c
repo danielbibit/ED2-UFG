d4d39f6e6b7e2f6e261727f9ffc8ba6364ab6a04
Daniel Moraes <danielbibit@gmail.com>
2017-12-01 00:54:33 +0000

    Implement node for the B-Tree

--- Trabalho_8/btree.c
+++ Trabalho_8/btree.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_KEYS (100)
+
+typedef struct node{
+	int is_leaf;
+	int n_keys;
+	int keys[MAX_KEYS];
+	struct node *children[MAX_KEYS+1];
+} node_t;
+
+
 int main(){
 
 }
