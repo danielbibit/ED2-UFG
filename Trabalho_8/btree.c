800f50897d3b47aa214d93696e6f2df0637998d6
Daniel Moraes <danielbibit@gmail.com>
2017-12-03 07:01:27 +0000

    start implementation of the insertion function

--- Trabalho_8/btree.c
+++ Trabalho_8/btree.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_KEYS (100)
+#define MAX_KEYS (5)
 
 typedef struct node{
 	int is_leaf;
 	int n_keys;
 	int keys[MAX_KEYS];
-	struct node *children[MAX_KEYS+1];
+	struct node *children[MAX_KEYS + 1];
 } node_t;
 
+node_t *empity_node(){
+	node_t *new_node;
+	new_node = malloc(sizeof(node_t));
+	new_node->n_keys = 0;
+}
+
+void insert_btree(node_t *root, int key){	
+	if(root == NULL){
+		node_t *new_node;
+		new_node = malloc(sizeof(node_t));
+
+		new_node->n_keys = 0;
+		new_node->keys[new_node->n_keys] = key;
+		new_node->n_keys += 1;
+		new_node->is_leaf = 1;
+	}else{	
+		if(root->n_keys < MAX_KEYS){
+		
+		}
+	}
+
+
+	
+}
 
 int main(){
-
+	node_t *tree;
+	insert_btree(tree, 10);
 }
